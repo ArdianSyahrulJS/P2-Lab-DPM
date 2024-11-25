1d06a66a101b7e625c96dd4c91b40fb15cfde3bc
ArdianSyahrulJS <akun37478@gmail.com>
2024-11-25 17:45:21 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,50 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Teks di tengah layar */}
+      <Text style={styles.text}>Ardian</Text>
+
+      {/* Kotak dengan tata letak FlexBox */}
+      <View style={styles.boxContainer}>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D3D3D3', 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24, 
+    color: 'blue', 
+    fontWeight: 'bold', 
+    marginBottom: 20, 
+  },
+  boxContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+    width: '80%', 
+    marginTop: 20, 
+  },
+  box1: {
+    width: 100, 
+    height: 100, 
+    backgroundColor: '#FF0000', // Merah
+  },
+  box2: {
+    width: 100, 
+    height: 100, 
+    backgroundColor: '#00FF00', // Hijau
+  },
+});
